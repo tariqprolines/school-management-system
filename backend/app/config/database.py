caf29f4044807aa2b4589fe440814dd0d5dbde32
Tariq <tariq.khan@onsumaye.com>
2026-08-25 15:10:52 +0530

    update

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -11,7 +11,6 @@ class Base(DeclarativeBase):
 engine = create_async_engine(settings.DATABASE_URL, echo=settings.DEBUG)
 async_session = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
-
 async def get_db():
     async with async_session() as session:
         yield session
